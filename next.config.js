9bf182f10405258d2a64be7275c69e0b0e84ad73
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-21 00:29:02 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   loader: 'akamai',
-  //   path: '',
-  // },
-  // assetPrefix: './',
 }
 
 module.exports = nextConfig

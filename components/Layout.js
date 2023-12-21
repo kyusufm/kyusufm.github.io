bbdfbba410930e6bb3ca3f85e10ce2695aadb356
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-21 23:15:33 +0700

    New update

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,28 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import {Sora}
+ from '@next/font/google'
+
+ //font settings
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100','200','300','400','500','600','700','800']
+})
+
+//components
+import Nav from '../components/Nav'
+import Header from '../components/Header'
+import TopLeftimg from '../components/TopLeftimg'
+import Head from 'next/head'
+
+
+const Layout = ({children}) => {
+  return (
+    <div className = {`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}`}>
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 const TopLeftImg = () => {
   return <div>Top Left Image</div>;
 };

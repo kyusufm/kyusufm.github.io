04246405c6ee162e6500248e772f00f729aecc67
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-23 04:01:27 +0700

    component

--- components/Layout.js
+++ components/Layout.js
@@ -17,7 +17,7 @@ import Head from 'next/head'
 
 const Layout = ({children}) => {
   return (
-    <div className = {`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}`}>
+    <div className = {`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
       <Nav />
       <Header />
       {children}

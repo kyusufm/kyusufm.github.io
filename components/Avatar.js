7305be1575afd2604e9a59a993b9f6ebccbf723c
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-26 15:25:10 +0700

    add particle,  transition, index, avatar, and button

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,13 @@
+// next image
+import Image from "next/image";
+
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return(
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image src={"/kyusufm.png"} width={737} height={678} alt="avatar image" className="translate-z-0 w-full h-full" />
+    </div>
+  );
 };
 
 export default Avatar;
--- components/Transition.js
+++ components/Transition.js
@@ -19,7 +19,27 @@ const transitionVariants = {
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className='fixed top-0 bottom-0 right-full h-screen z-30 bg-[#2e2257]' 
+      variants={transitionVariants} 
+      initial='initial' 
+      animate='animate' 
+      exit='exit' 
+      transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
+      ></motion.div>
+      <motion.div className='fixed top-0 bottom-0 right-full h-screen z-20 bg-[#2e2271]' 
+      variants={transitionVariants} 
+      initial='initial' 
+      animate='animate' 
+      exit='exit' 
+      transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}
+      ></motion.div>
+      <motion.div className='fixed top-0 bottom-0 right-full h-screen z-10 bg-[#4b3792]' 
+      variants={transitionVariants} 
+      initial='initial' 
+      animate='animate' 
+      exit='exit' 
+      transition={{delay: 0.6, duration: 0.6, ease: 'easeInOut'}}
+      ></motion.div>
     </>
   );
 };

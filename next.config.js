8ea23c12e42496813d8e0ed3ef5454eda87a1809
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-26 22:37:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -32,3 +32,21 @@
 //   // Optional: Set trailingSlash to true if you want to include trailing slashes in the exported paths
 //   // trailingSlash: true,
 // };
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig

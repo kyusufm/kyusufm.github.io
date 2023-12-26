a549bf0912e54b6b752fc8d0517b310fbd5046a8
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-26 18:14:55 +0700

    finish about page untill about data display

--- pages/about/index.js
+++ pages/about/index.js
@@ -114,21 +114,42 @@ const About = () => {
       className="hidden xl:flex absolute bottom-0 -left-[370px]">
       <Avatar />
     </motion.div>
-    <div className="container mx-auto h-full flex flex-col items-center xl:flex-row">
-      <div>text</div>
-      <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-        {aboutData.map((item, itemIndex) => {
-          return (
-          <div key={itemIndex} 
-          className={`${index=== itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-          onClick={() => setIndex(itemIndex)}
-          > 
-          {item.title}
-          </div>
-          );
-        })}
+    <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+      <div className="flex-1 flex flex-col justify-center">text</div>
+      <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]"> 
+        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+          {aboutData.map((item, itemIndex) => {
+            return (
+            <div 
+            key={itemIndex} 
+            className={`${
+              index=== itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+            onClick={() => setIndex(itemIndex)}
+            > 
+              {item.title}
+            </div>
+            );
+          })}
+        </div>
+        <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+          {aboutData[index].info.map((item, itemIndex) => {
+            return (
+              <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+                {/* title */}
+                <div>{item.title}</div>
+                <div className="hidden md:flex">-</div>
+                <div>{item.stage}</div>
+                {/* icons   */}
+                {item.icons?.map((icon, itemIndex) => {
+                  return <div className="text-2xl">{icon}</div>
+                })}
+              </div>
+            );
+          })}
+      </div>
       </div>
     </div>
+    
   </div>
   );
 };

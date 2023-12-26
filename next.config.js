e0f2a5402a4f6ee6dc377592f7d0499283ae733d
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-26 22:26:25 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,26 +9,26 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  /* config options here */
-  output: 'export',
-}
+// const nextConfig = {
+//   /* config options here */
+//   output: 'export',
+// }
  
-module.exports = {
-  // Other configurations...
+// module.exports = {
+//   // Other configurations...
 
-  // Add the exportPathMap function to define the paths to be exported
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      // Add other paths as needed
-    };
-  },
+//   // Add the exportPathMap function to define the paths to be exported
+//   exportPathMap: async function () {
+//     return {
+//       '/': { page: '/' },
+//       // Add other paths as needed
+//     };
+//   },
 
-  // Optional: Customize other export settings as needed
-  // For example, you can set basePath if your app is not hosted at the root of the domain
-  // basePath: '/your-base-path',
+//   // Optional: Customize other export settings as needed
+//   // For example, you can set basePath if your app is not hosted at the root of the domain
+//   // basePath: '/your-base-path',
 
-  // Optional: Set trailingSlash to true if you want to include trailing slashes in the exported paths
-  // trailingSlash: true,
-};
+//   // Optional: Set trailingSlash to true if you want to include trailing slashes in the exported paths
+//   // trailingSlash: true,
+// };

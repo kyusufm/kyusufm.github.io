b999427b30d81d2df846bdd3b51826c3ef2f234a
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-29 15:42:51 +0700

    Finish service section
    finish service section, will update the wording later

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -75,14 +75,14 @@ const ServiceSlider = () => {
             <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
               {/* icon */} 
               <div className="text-4xl text-accent mb-4">{service.icon}</div>
-              {/* title  */}
-              <div>
-                <div>{service.title}</div>
-                <div>{service.description}</div>
+              {/* title  and description*/}
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{service.title}</div>
+                <p className="max-w-[350px] leading-normal">{service.description}</p>
               </div>
               {/* arrow */}
               <div className="text-3xl">
-                <RxArrowTopRight />
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300"/>
               </div>
             </div>
             
--- pages/services/index.js
+++ pages/services/index.js
@@ -25,17 +25,33 @@ const Services = () => {
         <div className='flex flex-col xl:flex-row gap-x-8'>
           {/* text */}
           <div className='text-center flex xl:w=[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <h2 className='h2 xl:mt-8'>
+            <motion.h2 
+            variants={fadeIn('up', 0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2 xl:mt-8'>
               My services<span className='text-accent'>.</span>
-            </h2>
-            <p className='mb-4 max-w-[400px] mx:auto lg:mx-0'>
+            </motion.h2>
+            <motion.p 
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className='mb-4 max-w-[400px] mx:auto lg:mx-0'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+            </motion.p>
           </div>
-          <div className="w-full xl:max-w-[65%]">
-            {/* slider  */}
+          {/* slider  */}
+          <motion.div 
+            variants={fadeIn('down', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="w-full xl:max-w-[65%]">
+            
             <ServiceSlider />
-          </div>
+          </motion.div>
           
         </div>
       </div>

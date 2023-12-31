31a7f254a1e0658409d13fef4d170aca9c73c424
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-31 13:08:43 +0700

    work aded

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,4 +1,20 @@
-// data
+//import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+//import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// icons
+import {
+  BsArrowRight,
+  RxArrowTopRight,
+} from "react-icons/rx";
+
+// import require modules
+import { Pagination } from "swiper";
+import { space } from "postcss/lib/list";
 const workSlides = {
   slides: [
     {
@@ -45,7 +61,35 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper 
+    breakpoints={{
+      320:{
+        slidesPerView: 1,
+        spacceBetween:15,
+      },
+      640:{
+        slidesPerView: 3,
+        spaceBetween:15,
+      }
+    }}
+    FreeMode={true}
+    pagination ={{
+      clickable: true,
+    }}
+    modules={[Pagination]}
+    className="h-[240px] sm:h-[340px]">
+      {
+        WorkSlider.slides.map((item, index) => {
+          return(
+            <SwiperSlide key={index}>
+            
+            
+            </SwiperSlide>
+          );
+      })}
+      </Swiper>
+    );
 };
 
 export default WorkSlider;
